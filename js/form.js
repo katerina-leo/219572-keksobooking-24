0de654b29591c9c5a6a1f6d4d7d5d7c7c0e0c2b5
katerina-leo <81039066+katerina-leo@users.noreply.github.com>
2021-10-27 13:33:24 +0300

    Исправлены замечания

--- js/form.js
+++ js/form.js
@@ -4,6 +4,10 @@ const mapFiltersElement = document.querySelector('.map__filters');
 const selectMapFiltersElements = mapFiltersElement.querySelectorAll('select');
 const roomsSelectElement = adFormElement.querySelector('#room_number');
 const capacitySelectElement = adFormElement.querySelector('#capacity');
+const formElement = document.querySelector('#form');
+const selectElements = document.querySelectorAll('select');
+const inputElements = document.querySelectorAll('input');
+const submitButtonEl = document.querySelector('.ad-form__submit');
 
 const setDisabled = () => {
   adFormElement.classList.add('ad-form--disabled');
@@ -26,16 +30,17 @@ const setActive = () => {
   });
 };
 
-//Валидация Количество комнат-количество гостей
-const currentState = {
-  countRoom: null,
+//Валидация Количество комнат-количество гостей//
+
+const currentRoomState = {
+  countRoom: roomsSelectElement.value,
 };
 
 const checkCapacitySelect = (value) => {
 
   const valueToNumber = parseInt(value, 10);
 
-  if (valueToNumber > currentState.countRoom && valueToNumber !== 0) {
+  if (valueToNumber > currentRoomState.countRoom && valueToNumber !== 0) {
     capacitySelectElement.setCustomValidity('Нужно больше комнат');
   } else if (valueToNumber === 0) {
     capacitySelectElement.setCustomValidity('Нужно выбрать 100 комнат');
@@ -48,9 +53,51 @@ const checkCapacitySelect = (value) => {
 
 const setCountRooms = (value) => {
   const valueToNumber = parseInt(value, 10);
-  currentState.countRoom = valueToNumber;
+  currentRoomState.countRoom = valueToNumber;
 };
 capacitySelectElement.addEventListener('change', (evt) => checkCapacitySelect(evt.target.value));
 roomsSelectElement.addEventListener('change', (evt) => setCountRooms(evt.target.value));
+
+
+const currentState = {
+  isFailSend: false,
+};
+
+//Валидация формы//
+
+const setErrorBorder = () => {
+  const paintBorder = (el) => {
+    if (el.validity.valid) {
+      el.style.borderColor = 'white';
+    } else {
+      el.style.color =  'red';
+    }
+  };
+
+  selectElements.forEach((el) => {
+    paintBorder(el);
+  });
+
+  inputElements.forEach((el) => {
+    paintBorder(el);
+  });
+};
+
+formElement.addEventListener('change', () => {
+  if (currentState.isFailSend) {
+    setErrorBorder();
+  }
+});
+
+submitButtonEl.addEventListener('click', () => {
+  const isValidForm = formElement.checkValidity();
+
+  if (isValidForm) {
+    currentState.isFailSend = false;
+  } else {
+    currentState.isFailSend = true;
+    setErrorBorder();
+  }
+});
 
 export { setActive, setDisabled };
