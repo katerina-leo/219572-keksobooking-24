81371842d41d7887bbb9d758692e7f1044e9b65a
katerina-leo <81039066+katerina-leo@users.noreply.github.com>
2021-11-22 15:21:18 +0300

    Исправлены замечания по загрузке фото

--- js/img.js
+++ js/img.js
@@ -7,6 +7,9 @@ const previewPhotoContainer = adForm.querySelector('.ad-form__photo');
 
 const getLoadFile = (inputFile, preview ) => {
   const file = inputFile.files[0];
+  if (!file) {
+    return;
+  }
   const fileName = file.name.toLowerCase();
 
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
@@ -19,7 +22,6 @@ avatarChooser.addEventListener('change', () => {
   getLoadFile(avatarChooser, previewAvatar );
 });
 photoChooser.addEventListener('change', () => {
-  // previewPhotoContainer.innerHTML = '';
   const photo = document.createElement('img');
   getLoadFile(photoChooser, photo);
   photo.width = 70;
